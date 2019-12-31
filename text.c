bcaf96301e74e6b448385edfe13cf50a58882138
Sean McBride <spmcbride1201@gmail.com>
2019-12-31 14:29:21 -0500

    feat: Update test source

--- text.c
+++ text.c
@@ -2,16 +2,43 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_BUF (1024 * 1024) //1m
+#define CPU_CYCS 2100
+#define MAX_BUF (1024 * 1024 * 1) //1m
 
-int main(int argc, char **argv)
+#define ITERS_15US 125500
+#define MULTIPLE 5
+
+#define SPIN_ITERS (ITERS_15US * MULTIPLE)
+
+__attribute__((optnone)) static void
+wrk(void)
+{
+    unsigned int spin = 0;
+
+    while (spin < SPIN_ITERS)
+    {
+        //__asm__ __volatile__("nop": : :"memory");
+        spin++;
+    }
+}
+
+//__attribute__((optnone)) int
+int main(void)
 {
     char *d = malloc(MAX_BUF + 1);
     int r = read(0, d, MAX_BUF);
-    if (r <= 0)
-        printf("%s\n", r == 0 ? "empty" : "error");
+
+    //	unsigned long long st = rdtsc(), en = 0;
+    //	wrk();
+    //	en = rdtsc();
+
+    //	if (r <= 0) printf("%llu\n", en > st ? (en - st)/CPU_CYCS : -1);
+    if (r < 0)
+        printf("E\n");
+    else if (r <= 1)
+        printf("D\n");
     else
-        write(1, d, MAX_BUF);
+        write(1, d, r);
 
     return 0;
 }
