2e4029e31671309858600008ab9a0005000f5cdd
Sean McBride <spmcbride1201@gmail.com>
2019-12-30 20:04:54 -0500

    test: main test

---
+++ text.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_BUF (1024 * 1024) //1m
+
+int main(int argc, char **argv)
+{
+    char *d = malloc(MAX_BUF + 1);
+    int r = read(0, d, MAX_BUF);
+    if (r <= 0)
+        printf("%s\n", r == 0 ? "empty" : "error");
+    else
+        write(1, d, MAX_BUF);
+
+    return 0;
+}
